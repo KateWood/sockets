6b1f239a03d41d99e8f8f56207e389909d8b29aa
Kate Wood <katewood611@gmail.com>
2015-11-19 14:51:20 -0800

    adds io to server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 var express		= require('express'),
 	app			= express(),
 	logger		= require('morgan'),
-	server		= require('http').createServer(app)
+	server		= require('http').createServer(app),
+	port 		= process.env.PORT || 3000
+
+app.use(logger('dev'))
+
+
+
+server.listen(port, function() {
+	console.log('Server is running on port', port)
+})
+var io = require('socket.io')(server)
