c2ffb0683dc67130920a2e46b8e12055cd624d5c
Kate Wood <katewood611@gmail.com>
2015-11-19 15:20:12 -0800

    moves twitter var for correct placement

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var express	= require('express'),
 
 app.use(logger('dev'))
 
+
+server.listen(port, function() {
+	console.log('Server is running on port', port)
+})
+var io = require('socket.io')(server)
 var twitter = new Twit({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
 	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 	access_token: process.env.TWITTER_ACCESS_TOKEN,
 	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
-
-server.listen(port, function() {
-	console.log('Server is running on port', port)
-})
-var io = require('socket.io')(server)
