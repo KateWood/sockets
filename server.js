6149ebcaf893944514be68cc81c5480fca729125
Kate Wood <katewood611@gmail.com>
2015-11-19 15:19:11 -0800

    adds twitter key references to env variables

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-var express		= require('express'),
-	app			= express(),
-	logger		= require('morgan'),
-	server		= require('http').createServer(app),
-	port 		= process.env.PORT || 3000
+var express	= require('express'),
+	app		= express(),
+	logger	= require('morgan'),
+	server	= require('http').createServer(app),
+	port 	= process.env.PORT || 3000,
+	Twit 	= require('twit')
 
 app.use(logger('dev'))
 
-
+var twitter = new Twit({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token: process.env.TWITTER_ACCESS_TOKEN,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
 
 server.listen(port, function() {
 	console.log('Server is running on port', port)
